85cebdbcdf5e6aeeb569765f6352fd2bcab09f41
sankalpjain99 <sankalpjain99@gmail.com>
2020-12-26 11:47:04 +0530

    Fixed Bug in index.js line 2

--- js/index.js
+++ js/index.js
@@ -1,36 +1,38 @@
-if(!localStorage.getItem("token")===null){
-    window.location.href = "./tasks.html"
-}
-else{
-    document.getElementById("login-btn").addEventListener("click" , () => {
-        var email = document.getElementById("email").value;
-        var password = document.getElementById("pass").value;
-        fetch("https://sankalp-task-manager-api.herokuapp.com/users/login",{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({email: email, password:password})
-        }).then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            window.localStorage.setItem('token', data.token);
-            window.location.href = "./tasks.html"
+window.onload = () => {
+    if(!(localStorage.getItem("token")===null)){
+        window.location.href = "./tasks.html"
+    }
+    else{
+        document.getElementById("login-btn").addEventListener("click" , () => {
+            var email = document.getElementById("email").value;
+            var password = document.getElementById("pass").value;
+            fetch("https://sankalp-task-manager-api.herokuapp.com/users/login",{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email: email, password:password})
+            }).then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                window.localStorage.setItem('token', data.token);
+                window.location.href = "./tasks.html"
+            })
+            .catch((err) => console.log(err))
         })
-        .catch((err) => console.log(err))
-    })
-    
-    document.getElementById('get-user').addEventListener('click', () => {
-        fetch("https://sankalp-task-manager-api.herokuapp.com/users/me",{
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-            }
-        }).then((res) => res.json())
-        .then((data) => {
-            console.log(data)
+        
+        document.getElementById('get-user').addEventListener('click', () => {
+            fetch("https://sankalp-task-manager-api.herokuapp.com/users/me",{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+                }
+            }).then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((err) => console.log(err))
         })
-        .catch((err) => console.log(err))
-    })
+    }
 }
