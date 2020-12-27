2c415831939beabcc7a0ab5e9336aa24cd3ffa4c
sankalpjain99 <sankalpjain99@gmail.com>
2020-12-27 12:54:52 +0530

    Added Error Handling for SignUp.html

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,6 @@
 document.getElementById("signup-btn").addEventListener("click", () => {
     var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("pass").value;
     fetch("https://sankalp-task-manager-api.herokuapp.com/users",{
@@ -7,12 +8,16 @@ document.getElementById("signup-btn").addEventListener("click", () => {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({name: name, email: email, password:password})
+        body: JSON.stringify({name: name, age: age, email: email, password:password})
     }).then((res) => res.json())
     .then((data) => {
-        console.log(data);
-        window.localStorage.setItem('token', data.token);
-        window.location.href = "./tasks.html";
+        if(data.errors){
+            throw new Error(data.message.split(":").pop());
+        }
+        else{
+            window.localStorage.setItem('token', data.token);
+            window.location.href = "./tasks.html";
+        }
     })
-    .catch((err) => console.log(err))
+    .catch((err) => alert(err))
 })
