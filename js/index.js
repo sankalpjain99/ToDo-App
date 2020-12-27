df5253bffe36b4cb7225d381ca2374299a45f539
sankalpjain99 <sankalpjain99@gmail.com>
2020-12-27 12:34:32 +0530

    Added Error Handling to index.html

--- js/index.js
+++ js/index.js
@@ -6,20 +6,30 @@ window.onload = () => {
         document.getElementById("login-btn").addEventListener("click" , () => {
             var email = document.getElementById("email").value;
             var password = document.getElementById("pass").value;
+            if(email==="")
+                return alert("Email is Required");
+            if(password==="")
+                return alert("Password is Required");   
             fetch("https://sankalp-task-manager-api.herokuapp.com/users/login",{
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({email: email, password:password})
-            }).then((res) => res.json())
+            }).then((res) => {
+                if(res.status === 400){
+                    throw new Error("Invalid Email or Password");
+                }else{
+                    return res.json();
+                }
+            })
             .then((data) => {
-                console.log(data);
                 window.localStorage.setItem('token', data.token);
                 window.location.href = "./tasks.html"
             })
-            .catch((err) => console.log(err))
+            .catch((err) => alert(err))
         })
+
         document.getElementById("sign-btn").addEventListener("click", () => {
             window.location.href = "./signup.html";
         })
